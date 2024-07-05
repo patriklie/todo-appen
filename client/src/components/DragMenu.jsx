87dfdde2a9a95ffb166d8be4a1f437b02147dfc9
Patrik Lie <patrik.lie@hotmail.com>
2024-07-05 22:48:25 +0200

    onclick stop propagation

--- client/src/components/DragMenu.jsx
+++ client/src/components/DragMenu.jsx
@@ -117,6 +117,7 @@ const DragMenu = () => {
         >
          
           <div 
+          onClick={(e) => e.stopPropagation()}
           onPointerDown={(e) => {
             e.stopPropagation();
             controls.start(e);

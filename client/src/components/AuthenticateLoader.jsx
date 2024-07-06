7f9c2aa07f45dc73332ed5b5d7975eeaffe4b964
Patrik Lie <patrik.lie@hotmail.com>
2024-07-06 22:25:11 +0200

    fixed auth token icon loop

--- client/src/components/AuthenticateLoader.jsx
+++ client/src/components/AuthenticateLoader.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import IconContainer from './IconContainer';
+import React, { useEffect, useRef } from 'react';
+
 import Navbar from '../components/Navbar';
 import maskottLiten from '../assets/images/Maskott-utklipp.png';
+import { Player } from '@lordicon/react';
+
+const ICON = require(`../assets/lordicons/wired-outline-213-arrow-2-rounded-black.json`);
 
 const AuthenticateLoader = () => {
+
+  useEffect(() => {
+    playerRef.current?.playFromBeginning();
+  }, []);
+  const playerRef = useRef(null);
+ 
   return (
     <>
     <Navbar />
     <div className='loader-list-container'>
-        <IconContainer iconName={"wired-outline-213-arrow-2-rounded-black"} reveal={"loop-cycle"} hover={"hover-rotation"} size={50}  />
+    <Player
+        ref={playerRef} 
+        icon={ICON}
+        size={96}
+        onComplete={() => playerRef.current?.playFromBeginning()}
+        state={"loop-cycle"}
+      />
         <div className='load-list-text'>Godkjenner token...</div>
         <img className='liten-maskott' src={maskottLiten} />
     </div>
